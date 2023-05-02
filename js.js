a5cac0c3d7c43ecf97b1f96324dc2a02425d4746
huseyn <you@example.com>
2023-05-02 21:19:28 +0500

    Перевод на таджиксном

--- js.js
+++ js.js
@@ -1,6 +1,83 @@
-document.getElementById("Learn").onclick = function () {
-    document.getElementById("help").scrollIntoView({ behavior: "smooth" });
+document.getElementById("Learn").onclick = function(){
+    document.getElementById("help").scrollIntoView({behavior:"smooth"});
 }
-document.getElementById("donate1").onclick = function () {
-    document.getElementById("help").scrollIntoView({ behavior: "smooth" });
-}   
+var buttons = document.getElementsByClassName("donate");
+for(var i=0;i<buttons.length;i++){
+    buttons[i].onclick = function(){
+    document.getElementById("help1").scrollIntoView({behavior:"smooth"});
+    }
+}
+document.getElementById("price-action").onclick = function(){
+if(document.getElementById("name").value === ""){
+    alert("Номнавис шавед")
+}
+else if
+    (document.getElementById("phone").value === ""){
+    alert("Телефони мобилиатонро гузоред")
+}
+else if
+    (document.getElementById("kard").value === ""){
+    alert("Корти пластикиатонро гузоред")
+}
+else if
+    (document.getElementById("price").value === ""){
+    alert("Нархатонро гузоред")
+}
+else{
+    alert("Рахмат, баъд аз ягон соат мо ба шумо занг мезанем!")
+    }
+}
+
+var buttons = document.getElementsByClassName("donate");
+for(var i=0;i<buttons.length;i++){
+    buttons[i].onclick = function(){
+    document.getElementById("help1").scrollIntoView({behavior:"smooth"});
+    }
+}
+document.getElementById("price-action1").onclick = function(){
+if(document.getElementById("name1").value === ""){
+    alert("Номнавис шавед")
+}
+else if
+    (document.getElementById("phone1").value === ""){
+    alert("Телефони мобилиатонро гузоред")
+}
+else if
+    (document.getElementById("kard1").value === ""){
+    alert("Корти пластикиатонро гузоред")
+}
+else if
+    (document.getElementById("price1").value === ""){
+    alert("Нархатонро гузоред")
+}
+else{
+    alert("Рахмат, баъд аз ягон соат мо ба шумо занг мезанем!")
+    }
+}
+
+var buttons = document.getElementsByClassName("donate");
+for(var i=0;i<buttons.length;i++){
+    buttons[i].onclick = function(){
+    document.getElementById("help1").scrollIntoView({behavior:"smooth"});
+    }
+}
+document.getElementById("price-action2").onclick = function(){
+if(document.getElementById("name2").value === ""){
+    alert("Номнавис шавед")
+}
+else if
+    (document.getElementById("phone2").value === ""){
+    alert("Телефони мобилиатонро гузоред")
+}
+else if
+    (document.getElementById("kard2").value === ""){
+    alert("Корти пластикиатонро гузоред")
+}
+else if
+    (document.getElementById("price2").value === ""){
+    alert("Нархатонро гузоред")
+}
+else{
+    alert("Рахмат, баъд аз ягон соат мо ба шумо занг мезанем!")
+    }
+}
